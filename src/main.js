4fefddb233f9f4756e00c95d7e877fd3bad274f3
BanariVlad <banari.vlad@yandex.com>
2020-07-03 10:25:25 +0300

    Filter

--- src/main.js
+++ src/main.js
@@ -4,6 +4,15 @@ import router from "./router";
 
 Vue.config.productionTip = false;
 
+Vue.filter('currency', value => {
+  if (isNaN(Number(value)) || value === "") {
+    return "0.00";
+  }
+
+  let formattedValue = parseFloat(value).toFixed(2);
+  return formattedValue.toString().replace(/\B(?=(\d{3})+(?!\B))/g, " ");
+});
+
 new Vue({
   router,
   render: h => h(App)
